3863aceed7458f6526191890c0bb60c028218865
Ganaga <ganaga@laposte.net>
2023-04-07 17:32:57 +0200

    Add possibility to update a dataset

--- api/src/map.ts
+++ api/src/map.ts
@@ -16,6 +16,7 @@ class Map extends Listener {
     private iframe: HTMLIFrameElement;
     private messenger: Messenger;
     private loaded: boolean = false;
+    private refreshId:string;
 
     /**
      * Creates the main Map and define its parameters.
@@ -29,20 +30,20 @@ class Map extends Listener {
         this.options = options;
         this.element = element;
 
-        if(!options.lang) options.lang = navigator.language.split('-')[0];
+        if (!options.lang) options.lang = navigator.language.split('-')[0];
 
         if (!options.url) options.url = 'https://showroom.galigeo.com/Galigeo';
         //options.url = 'http://localhost:8088/Galigeo';
-        if(options.id && !options.mapId) options.mapId = options.id; // handle legacy parameter
-        if(options.name && !options.mapId) options.mapId = options.name.toUpperCase().toLowerCase().replace(/[^a-z0-9]/gi, '');
+        if (options.id && !options.mapId) options.mapId = options.id; // handle legacy parameter
+        if (options.name && !options.mapId) options.mapId = options.name.toUpperCase().toLowerCase().replace(/[^a-z0-9]/gi, '');
 
         if (this.options.url.endsWith('/')) {
             this.options.url = this.options.url.slice(0, -1);
         }
-        if(!this.options.data) this.options.data = [];
+        if (!this.options.data) this.options.data = [];
 
-        for(let dataset of this.options.data) {
-            if(dataset.url) {
+        for (let dataset of this.options.data) {
+            if (dataset.url) {
                 dataset.url = this.fixRelativeUrl(dataset.url);
             }
         }
@@ -67,17 +68,18 @@ class Map extends Listener {
                 body: `status=200&lang=${navigator.language}&data=${encodeURIComponent(JSON.stringify(this.options))}`
             }).then(res => res.json())
                 .then(json => {
-                    if(!json.message) {
-                    this.iframe = this.createIframe(this.element, json);
-                    this.messenger = new Messenger(this.iframe);
-                    this.registerEvents();
-                    console.log('API- after fetch json', json);
-                    this.messenger.waitMapIsLoad().then(res => {
-                        console.log('API- Map loaded', json);
-                        this.loaded = true;
-                        this.setMapControlsVisible(false);
-                        resolve(this);
-                    });
+                    if (!json.message) {
+                        this.iframe = this.createIframe(this.element, json);
+                        this.messenger = new Messenger(this.iframe);
+                        this.refreshId = json.refreshId;
+                        this.registerEvents();
+                        console.log('API- after fetch json', json);
+                        this.messenger.waitMapIsLoad().then(res => {
+                            console.log('API- Map loaded', json);
+                            this.loaded = true;
+                            this.setMapControlsVisible(false);
+                            resolve(this);
+                        });
                     } else {
                         this.showError(json.message, null);
                     }
@@ -89,11 +91,40 @@ class Map extends Listener {
 
         });
     }
-    private showError(msg : string, err: any) {
+    /**
+     * Update the current map.
+     * Currently, only the data from MapParameters can
+     * be updated.
+     * 
+     * @param mapParameters 
+     */
+    public update(mapParameters: MapParameters) {
+        if(!this.refreshId || !this.loaded) throw new Error('Data update not available');
+        return new Promise((resolve, reject) => {
+            fetch(this.options.url + '/api/openMap/encoded?refreshId=' + this.refreshId, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+                },
+                body: `status=200&lang=${navigator.language}&data=${encodeURIComponent(JSON.stringify(mapParameters))}`
+            }).then(res => res.json()
+                .then(json => {
+                    if (!json.message) {
+                        resolve(json);
+                    } else {
+                        reject(json);
+                    }
+                })).catch(err => {
+                    this.showError('Unable to update data', err);
+                    reject(err)
+                });
+        });
+    }
+    private showError(msg: string, err: any) {
         console.error('Failed to load map', msg, err);
         // clean up dom when showing error
-        const root:HTMLElement = document.getElementById(this.element);
-        while(root.firstChild){
+        const root: HTMLElement = document.getElementById(this.element);
+        while (root.firstChild) {
             root.removeChild(root.firstChild);
         }
         root.insertAdjacentHTML('beforeend', `<div id="galigeoError" style="height:100%;
@@ -104,7 +135,7 @@ class Map extends Listener {
                font-size: x-large;">
             <h4>Unable to access Galigeo from ${this.options.url}</h4> 
             <p>${msg}</p>
-            <p>${err?err.toLocaleString():''}</p></div>`);
+            <p>${err ? err.toLocaleString() : ''}</p></div>`);
     }
     /**
      * 
@@ -117,9 +148,9 @@ class Map extends Listener {
      * Refresh the map. This function is useful to see data updated on the map.
      * 
      */
-     refresh() {
-		return this.messenger.postMessage('refresh', {});
-	}
+    refresh() {
+        return this.messenger.postMessage('refresh', {});
+    }
     /**
      * Set the extent of the map
      * @param extent The extent to set
@@ -128,48 +159,48 @@ class Map extends Listener {
         return this.messenger.postMessage('setExtent', extent);
     }
     /**
-	 * Zoom to a specific location using fly method
-	 * @param {Number} coordinates x coordinates to zoom in
+     * Zoom to a specific location using fly method
+     * @param {Number} coordinates x coordinates to zoom in
      * @param {Number} coordinates y coordinates to zoom in
-	 * @param {Number} zoom level used in the zoom
-	 */
-    flyTo(x:number, y: number, zoom: number) {
-        return this.messenger.postMessage('flyTo', {x, y, zoom});
+     * @param {Number} zoom level used in the zoom
+     */
+    flyTo(x: number, y: number, zoom: number) {
+        return this.messenger.postMessage('flyTo', { x, y, zoom });
     }
     /**
-	 * Zoom to a specific location without animations
-	 * @param {Number} coordinates x coordinates to zoom in
+     * Zoom to a specific location without animations
+     * @param {Number} coordinates x coordinates to zoom in
      * @param {Number} coordinates y coordinates to zoom in
-	 * @param {Number} zoom level used in the zoom
-	 */
-    zoomTo(x:number, y: number, zoom: number) {
-        return this.messenger.postMessage('zoomTo', {x, y, zoom});
+     * @param {Number} zoom level used in the zoom
+     */
+    zoomTo(x: number, y: number, zoom: number) {
+        return this.messenger.postMessage('zoomTo', { x, y, zoom });
     }
     /**
-	 * Take a screenshot of the map 
-	 * @param {String} title of screen saved 
+     * Take a screenshot of the map 
+     * @param {String} title of screen saved 
      * @param {Boolean} savePng save the png in your disc
-	 * @param {Boolean} withLegend add legend to print
-	 */
+     * @param {Boolean} withLegend add legend to print
+     */
     print(withLegend: boolean, savePng: boolean, title: string): Promise<any> {
         return new Promise((resolve, reject) => {
-        this.messenger.postMessage('print', {withLegend, savePng, title}).then((textBlob)=> {
-            resolve(textBlob) ;
+            this.messenger.postMessage('print', { withLegend, savePng, title }).then((textBlob) => {
+                resolve(textBlob);
+            });
         });
-    });
     }
     /**
     * enable navigation like zoom & pan ... when it's disabled 
     */
-	enableMapNavigation() {
-		return this.messenger.postMessage('enableMapNavigation', {});
-	}
+    enableMapNavigation() {
+        return this.messenger.postMessage('enableMapNavigation', {});
+    }
     /**
     * disable navigation like zoom & pan ... when it's enabled 
     */
-	disableMapNavigation() {
-		return this.messenger.postMessage('disableMapNavigation', {});
-	}
+    disableMapNavigation() {
+        return this.messenger.postMessage('disableMapNavigation', {});
+    }
     /**
      * Define the visibility of the vertical menu (top left of the viewer).
      * This option is useful to display a "minimal" embeded map.
@@ -197,25 +228,25 @@ class Map extends Listener {
         return this.messenger.postMessage('setOpenNewTabVisible', visible);
     }
     /**
-	 * Set the basemap on the map. The list of basemap parameters is described on the
-	 * main product {@link https://doc.galigeo.com/G21_0/GGO/USER_GUIDE/en/#t=Basemaps_Configuration.htm&rhsearch=basemap&rhhlterm=basemap&rhsyns=%20|documentation}.
-	 * @param {String}  basemap name of the basemap
-	 */
+     * Set the basemap on the map. The list of basemap parameters is described on the
+     * main product {@link https://doc.galigeo.com/G21_0/GGO/USER_GUIDE/en/#t=Basemaps_Configuration.htm&rhsearch=basemap&rhhlterm=basemap&rhsyns=%20|documentation}.
+     * @param {String}  basemap name of the basemap
+     */
     setBasemap(basemap: string) {
         return this.messenger.postMessage('setBasemap', basemap);
     }
-    addDataUrl(url:string, name:string) {
-        if(this.isLoaded()) throw new Error('Cannot add new data once the map is loaded');
+    addDataUrl(url: string, name: string) {
+        if (this.isLoaded()) throw new Error('Cannot add new data once the map is loaded');
         this.options.data.push({
-			format: 'link',
-			url: this.fixRelativeUrl(url),
-			name: name
-		});
+            format: 'link',
+            url: this.fixRelativeUrl(url),
+            name: name
+        });
     }
     /**
-	 * Add a array of geojson features to the map. This function adds a new layer on the map.
-	 * @param {Object[]} features 
-	 * - ex: lines [{
+     * Add a array of geojson features to the map. This function adds a new layer on the map.
+     * @param {Object[]} features 
+     * - ex: lines [{
       "type": "Feature",
       "properties": {
         "party": "abc",
@@ -236,7 +267,7 @@ class Map extends Listener {
         "coordinates": [[-100, 40], [-105, 45], [-110, 55]]
       }
     }]
-	* - ex: polygons {
+    * - ex: polygons {
       "type": "Feature",
       "properties": {
         "party": "Republican",
@@ -269,54 +300,54 @@ class Map extends Listener {
         ]]
       }
     }]
-	 * @param {Style | undefined} style a style of features
-	 * @returns layerId of the object in the map
-	 */
-    createGeojsonFeatures(features: any [], style: Style): Promise<Layer> {
+     * @param {Style | undefined} style a style of features
+     * @returns layerId of the object in the map
+     */
+    createGeojsonFeatures(features: any[], style: Style): Promise<Layer> {
         return new Promise((resolve, reject) => {
             this.messenger.postMessage('createGeojsonFeatures', { features, style }).then(res => {
-                const layer = new Layer(res.layer_id, {name: res.layer_id, isApiLayer: true, datasetId: undefined, datasourceId: undefined, visible: true, messenger: this.messenger});
+                const layer = new Layer(res.layer_id, { name: res.layer_id, isApiLayer: true, datasetId: undefined, datasourceId: undefined, visible: true, messenger: this.messenger });
                 resolve(layer);
             });
         });
     }
     /**
-	 * Create new marker in the position x,y with custom svg image or style
-	 * @example
-	 * var coordinates = [ event.lat, event.lng ];
-	 * var iconSvgOptions = { iconSize : [ 40, 40 ] };
-	 * var svgXml = `<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="map-marker" class="svg-inline--fa fa-map-marker fa-w-12" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path fill="${color}" d="M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0z"></path></svg>`;
-	 * var url = encodeURI("data:image/svg+xml," + svgXml).replace('#', '%23');
-	 * markerLayer = mapGaligeo.addMarker(coordinates, undefined, iconSvgOptions, svgXml);
-	 *
-	 * @param {Number[]} coordinates  [x,y] 
-	 * @param {Object | undefined} style 
-	 * @param {Object} iconSvgOptions ex: {iconSize: [40, 40]}
-	 * @param {String} svgUrl URL of the SVG
+     * Create new marker in the position x,y with custom svg image or style
+     * @example
+     * var coordinates = [ event.lat, event.lng ];
+     * var iconSvgOptions = { iconSize : [ 40, 40 ] };
+     * var svgXml = `<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="map-marker" class="svg-inline--fa fa-map-marker fa-w-12" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path fill="${color}" d="M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0z"></path></svg>`;
+     * var url = encodeURI("data:image/svg+xml," + svgXml).replace('#', '%23');
+     * markerLayer = mapGaligeo.addMarker(coordinates, undefined, iconSvgOptions, svgXml);
+     *
+     * @param {Number[]} coordinates  [x,y] 
+     * @param {Object | undefined} style 
+     * @param {Object} iconSvgOptions ex: {iconSize: [40, 40]}
+     * @param {String} svgUrl URL of the SVG
     
-	 * @returns layerLeaflet the marker object leaflet in the map
-	 */
-    addMarker(coordinates: number [], style: any, iconSvgOptions: any, urlSvg: any): Promise<Layer> {
+     * @returns layerLeaflet the marker object leaflet in the map
+     */
+    addMarker(coordinates: number[], style: any, iconSvgOptions: any, urlSvg: any): Promise<Layer> {
         return new Promise((resolve, reject) => {
             this.messenger.postMessage('addMarker', { coordinates, style, iconSvgOptions, urlSvg }).then(res => {
-                const layer = new Layer(res.layer_id, {name: res.layer_id, isApiLayer: true, datasetId: undefined, datasourceId: undefined, visible: true, messenger: this.messenger});
+                const layer = new Layer(res.layer_id, { name: res.layer_id, isApiLayer: true, datasetId: undefined, datasourceId: undefined, visible: true, messenger: this.messenger });
                 resolve(layer);
             });
         });
     }
     /**
-	 * Remove a layer on the map. This method works on marker/geojson layers created with the API,
+     * Remove a layer on the map. This method works on marker/geojson layers created with the API,
      * as well with standard Galigeo layers created from the map itself.
-	 * @param {String} Layer 
-	 */
+     * @param {String} Layer 
+     */
     removeLayer(layer: Layer): Promise<string> {
         return new Promise((resolve, reject) => {
             this.messenger.postMessage('removeLayer', { layer }).then(res => {
-                 resolve(res);
+                resolve(res);
             });
         });
     }
-    
+
     /**
      * Get the list of layers
      * @returns Promise<Layer[]>
@@ -326,7 +357,7 @@ class Map extends Listener {
             this.messenger.postMessage('getLayers', null).then(messageLayers => {
                 const layers: Layer[] = [];
                 for (const l of messageLayers) {
-                    const layer = new Layer(l.id, {name: l.name, datasetId: l.datasetId, datasourceId: l.datasourceId, visible: l.visible, messenger: this.messenger});
+                    const layer = new Layer(l.id, { name: l.name, datasetId: l.datasetId, datasourceId: l.datasourceId, visible: l.visible, messenger: this.messenger });
                     layers.push(layer);
                 }
                 resolve(layers);
@@ -338,49 +369,49 @@ class Map extends Listener {
      * Listen the events received from the messenger then propagate
      */
     private registerEvents() {
-        this.messenger.addEventListener('map', (msg:Message)=>{
+        this.messenger.addEventListener('map', (msg: Message) => {
             // adapt the object passed through the event to match API objects
-            let value:any;
-            if(msg.action === 'zoomend') {
+            let value: any;
+            if (msg.action === 'zoomend') {
                 value = new Extent(msg.value.minX, msg.value.minY, msg.value.maxX, msg.value.maxY);
             } else {
                 value = msg.value;
             }
-            this.fireEvent(msg.action, value);    
+            this.fireEvent(msg.action, value);
         });
-        
+
     }
 
     private createIframe(element: string, json: any): HTMLIFrameElement {
         const mapDiv = document.getElementById(element);
         const indexPage = this.options.devMode ? 'indexdev.jsp' : 'index.html';
-        
+
         // eventually remove the error page
-        const errorDiv:HTMLElement = document.getElementById("galigeoError");
-        if(errorDiv) {
+        const errorDiv: HTMLElement = document.getElementById("galigeoError");
+        if (errorDiv) {
             errorDiv.remove();
         }
 
         // build the map url
         let urlOptions = 'listenMessages=true';
-        if(this.options.crossDomain) urlOptions += '&crossDomain=true'
-        if(this.options.lang) urlOptions += '&lang=' + this.options.lang;
+        if (this.options.crossDomain) urlOptions += '&crossDomain=true'
+        if (this.options.lang) urlOptions += '&lang=' + this.options.lang;
         const serviceUrl = this.options.url + '/' + json.relativeUrlServiceUrl;
         let src = `${this.options.url}/viewer/${indexPage}?${urlOptions}&url=${serviceUrl}&lang=${navigator.language}`;
-        
+
         // sso ?
-        if(json.sso) {
+        if (json.sso) {
             src = `${this.options.url}/oauth/login.html?orgId=${json.orgId}&service=${json.service}&redirect=${encodeURIComponent(src)}`
         }
 
         // If redirect=true, then skip iframe creation
-        if(this.options.redirect) {
+        if (this.options.redirect) {
             window.location.href = src;
             return null;
         }
 
         let iframe: HTMLIFrameElement = document.getElementById('galigeoMap') as HTMLIFrameElement;
-        if(iframe) {
+        if (iframe) {
             iframe.src = src;
         } else {
             iframe = document.createElement('iframe');
@@ -400,7 +431,7 @@ class Map extends Listener {
         return iframe !== null;
     }
 
-    private fixRelativeUrl(url:string): string {
+    private fixRelativeUrl(url: string): string {
         var httpIdx = url.toLowerCase().indexOf('http');
         if (httpIdx !== 0) {
             // in case of relative path, build the full url
